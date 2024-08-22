0bcdebf60316aa096d9f2110e839c95231d4a438
martin <martin@normallydistributed.net>
2024-08-21 17:56:12 -0700

    begin EDA

---
+++ EDA.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(nflfastR)
+
+pbp = load_pbp(2023)
+
+passes = pbp %>%
+  filter(play_type == "pass")
+
+adot = passes %>%
+  group_by(passer_id, passer) %>%
+  summarize(count = n(),
+            adot = mean(air_yards, na.rm = T)) %>%
+  filter(count >= 100 & !is.na(passer)) %>%
+  arrange(desc(adot))
+
+ggplot(pbp, aes(x = passing_yards)) +
+  geom_histogram(binwidth = 1)
