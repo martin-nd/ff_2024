bb436540a652b03d42c5197060936f64930888e1
martin <martin@normallydistributed.net>
2024-08-21 23:53:32 -0700

    update eda

--- EDA.R
+++ EDA.R
@@ -38,5 +38,45 @@ joined = ag_pass_ts %>%
             by = c("passer_player_name",
                    "next_season",
                    "passer_player_id")) %>%
-  arrange(passer_player_id, passer_player_name, season)
+  arrange(passer_player_id, passer_player_name, season) %>%
+  filter(!is.na(season))
+
+model = lm(yga_next_season ~ yga, joined)
+
+ggplot(joined, aes(x = yga, y = yga_next_season)) +
+  geom_point(aes(color = passer_player_name)) +
+  geom_smooth(method = lm, se = T)
+
+plot(model)
+cor(joined[c("yga", "yga_next_season")])
+
+sack_set = passes %>%
+  filter(!is.na(sack))
+
+sack_set %>%
+  group_by(sack) %>%
+  summarize(count = n())
+
+sacks = sack_set %>%
+  filter(sack == 1)
+
+potential_sackers = c("sack",
+                      "yards_gained",
+                      "ydstogo",
+                      "score_differential",
+                      "qb_dropback",
+                      "qb_kneel", "game_seconds_remaining")
+
+corrplot::corrplot(cor(sack_set[potential_sackers]))
+
+commons = sack_set %>%
+  group_by(passer_player_id) %>%
+  summarize(n = n()) %>%
+  filter(n >= 50) %>%
+  .$passer_player_id
+
+oneway.test(sack ~ passer_player_id,
+            data = sack_set %>%
+              filter(passer_player_id %in% commons),
+            var.equal = F)
 
